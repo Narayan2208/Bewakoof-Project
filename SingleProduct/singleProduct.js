fd65bb072cae5c86769b2622966aae583fa786fc
SurajAnand88 <surajbloodstone@gmail.com>
2023-02-02 02:21:15 +0530

    completed single product page

--- SingleProduct/singleProduct.js
+++ SingleProduct/singleProduct.js
@@ -1,13 +1,16 @@
-// async function getData() {
-//   const jsondata = await fetch("https://bewakoof-api.onrender.com/mens");
-//   const data = await jsondata.json();
-//   var d = JSON.stringify(data);
-//   console.log(data);
-//   // console.log(d);
+import desc from "./desc.js";
+import offers from "./offers.js";
 
-//   displayData(data[0]);
-// }
-// getData();
+async function getData() {
+  const jsondata = await fetch("https://bewakoof-api.onrender.com/mens");
+  const data = await jsondata.json();
+  var d = JSON.stringify(data);
+  // console.log(data);
+  // console.log(d);
+
+  displayData(data[3]);
+}
+getData();
 
 var d = {
   image:
@@ -36,15 +39,23 @@ var d = {
   sleeve: "half",
 };
 
-console.log(d);
-displayData(d);
+// console.log(d);
+// displayData(d);
 function displayData(element) {
   document.getElementById("product").innerHTML = `
-    <div class="col-md-6 ">
-        <div class="row">
-            <div id="img-sl" class="col-2 d-none d-md-block " ></div>
+    <div class="col-md-6 ps-md-5">
+        <div class="row ">
+            <div id="imgsl" class="d-flex col-2 d-none d-md-block mt-2">
+                <p class="ms-4"><i class="fa-solid fa-2x fa-angle-up  ms-xl-1 ms-xxl-2"></i></p>
+                <div><img src="${element.image}" class="img-fluid w-100 "></div>
+                <div><img src="${element.image2}" class="img-fluid w-100 mt-2"></div>
+                <div><img src="${element.image3}" class="img-fluid w-100 mt-2"></div>
+                <div><img src="${element.image4}" class="img-fluid w-100 mt-2"></div>
+                <div><img src="${element.image}" class="img-fluid w-100 mt-2" id="image-last"></div>
+                <p class="ms-4"><i class="fa-solid fa-2x fa-angle-down mt-4 ms-xl-1 ms-xxl-2"></i></p>
+            </div>
             <div class="col " >
-                <img src="${element.image}" class="img-fluid w-100 onclick="(image(${element.price}))">
+                <img src="${element.image}" class="img-fluid w-100 " id="main-image">
             </div>
         </div>
     </div>
@@ -59,14 +70,14 @@ function displayData(element) {
         <p>inclusive of all taxes</p>
         
         <div class="d-flex">
-            <p class="ms-3  px-2 py-1 text-white fw-bold bg-secondary">${element.tag}</p>
+            <p class="ms-3  px-2 py-1 text-white fw-bold bg-secondary" id="tag">${element.tag}</p>
             <p class="ms-4 border border-dark px-2 py-1 fw-bold">DESIGN OF THE WEEK</p>
         </div>
 
         <div>
             <hr>
-                <p>
-                TriBe members get an extra discount of <span class="fw-bold">₹40</span> and FREE shipping.</span><a>Learn more</a></span>
+                <p class="pe-md-5">
+                TriBe members get an extra discount of <span class="fw-bold">₹40</span> and FREE shipping.</p><a>Learn more</a></p>
                 </p>
             <hr>
         </div>
@@ -76,7 +87,7 @@ function displayData(element) {
             <a class="col-6 text-end fw-bold" style="color:#42A2A2">Size Guide</a>
         </div>
 
-        <div class="row row-cols-10 m-3 py-2 pb-4">
+        <div class="row row-cols-10 m-3 py-2 pb-4" id="size">
             <div class="col p-3 border border-dark text-center ms-3 rounded-3"> S</div>
             <div class="col p-3 border border-dark text-center ms-3 rounded-3"> M</div>
             <div class="col p-3 border border-dark text-center ms-3 rounded-3"> L</div>
@@ -89,8 +100,8 @@ function displayData(element) {
             <div class="col p-2 ms-3"></div>
         </div>
 
-        <div class="row mx-3">
-            <button class="col me-1 border-0 fw-bold " style="font-size:14px; background-color:#FFD84D; padding: 10px 0"><i class="fa-solid fa-bag-shopping me-2"></i>ADD TO BAG</button>
+        <div class="row mx-3" id="buttons">
+            <button class="col me-1 border-0 fw-bold " style="font-size:14px; background-color:#FFD84D; padding: 10px 0" ><i class="fa-solid fa-bag-shopping me-2"></i>ADD TO BAG</button>
             <button class="col ms-1 border border-dark bg-light fw-bold opacity-50" style="font-size:14px; padding: 10px 0"><i class="fa-regular fa-heart me-2"></i>WISHLIST</button>
         </div>
 
@@ -105,7 +116,7 @@ function displayData(element) {
 
             <div class="border-0 collapse mt-0" id="offers">
                 <div class="card card-body py-0 border-0 px-4">
-                Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+                 
                 </div>
             </div>
             
@@ -132,7 +143,7 @@ function displayData(element) {
 
             <div class="border-0 collapse mt-0" id="exchange">
                 <div class="card card-body py-0 border-0 px-4">
-                Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+                Easy returns upto 15 days of delivery. Exchange available on select pincodes.
                 </div>
             </div>
 
@@ -151,10 +162,53 @@ function displayData(element) {
             
        </div>
     </div>
-    
     `;
-}
 
-function image(i) {
-  console.log(i);
+  // getting tag
+  let tag = document.getElementById("tag");
+  if (tag.textContent == "undefined") {
+    document.getElementById("tag").textContent = "BUY 3 FOR 899";
+  }
+  if (element.image1 !== undefined) {
+    document.getElementById("image-last").src = element.image1;
+  }
+
+  //setting extra images
+  document.getElementById("imgsl").addEventListener("click", function (e) {
+    document.getElementById("main-image").src = e.target.src;
+  });
+
+  //adding to cart
+  document.querySelector("#buttons").addEventListener("click", function (e) {
+    if (e.target.textContent === "ADD TO BAG") {
+      const obj = JSON.parse(localStorage.getItem("viewProduct"));
+      var cart = JSON.parse(localStorage.getItem("cart")) || [];
+      cart.push(obj);
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  });
+
+  //selecting sizes
+  let sizes = document.querySelectorAll("#size div");
+  for (let i = 0; i < 6; i++) {
+    sizes[i].addEventListener("click", () => {
+      for (let j = 0; j < 6; j++) {
+        if (
+          window.getComputedStyle(sizes[j]).backgroundColor === "rgb(0, 0, 0)"
+        ) {
+          sizes[j].style.backgroundColor = "rgba(0, 0, 0, 0)";
+          sizes[j].style.color = "black";
+        }
+      }
+      if (
+        window.getComputedStyle(sizes[i]).backgroundColor == "rgba(0, 0, 0, 0)"
+      ) {
+        sizes[i].style.backgroundColor = "black";
+        sizes[i].style.color = "white";
+      }
+    });
+  }
+
+  document.querySelector("#offers div").innerHTML = offers();
+  document.querySelector("#description div").innerHTML = desc();
 }
