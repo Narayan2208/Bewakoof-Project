9a2d8e51a6201cd9d3374579321b86d13b9b46ad
Amardesai5555 <amardesai5555@gmail.com>
2023-02-04 11:07:31 +0530

    Final

--- home/script/men.js
+++
@@ -1,9 +0,0 @@
-import {navbar2} from "../componenets/navbar.js";
-
-let navbar1 = document.querySelector("nav");
-navbar1.innerHTML = navbar2();
-
-import footer from "../componenets/footer.js";
-
-let footeradd = document.querySelector("footer");
-footeradd.innerHTML = footer();
